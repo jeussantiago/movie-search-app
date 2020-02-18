9456f09d36dd7d5c97c656976c93255e20944ef3
Jeus Santiago <jeus1santiago@gmail.com>
2020-02-18 16:17:01 -0500

    Updates

---
+++ precache-manifest.74d173ba170a5f4e3235bfe098165a0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a9876776de6c4c437d1ca5d5ec7c0f2",
+    "url": "/movie-search-app/index.html"
+  },
+  {
+    "revision": "d2e734f05d3e00e03e07",
+    "url": "/movie-search-app/static/css/2.c0f5ac91.chunk.css"
+  },
+  {
+    "revision": "f6f4bc301da5b7f98caa",
+    "url": "/movie-search-app/static/css/main.ce86c0d2.chunk.css"
+  },
+  {
+    "revision": "d2e734f05d3e00e03e07",
+    "url": "/movie-search-app/static/js/2.6677a6c0.chunk.js"
+  },
+  {
+    "revision": "f6f4bc301da5b7f98caa",
+    "url": "/movie-search-app/static/js/main.b02b8dcf.chunk.js"
+  },
+  {
+    "revision": "4fc3701c7b340cdd33ed",
+    "url": "/movie-search-app/static/js/runtime-main.0e22e803.js"
+  }
+]);
